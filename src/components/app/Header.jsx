50acd8a2b6607e0640e37bf931eeca69d43e7c56
culi <culitif@tuta.io>
2021-08-10 15:35:17 -0700

    add search functionality

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './header.css';
+import Search from './Search';
 
-const Header = () => {
+const Header = props => {
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.setSearchTerm(e.target.search.value);
+  };
+
   return (
     <header className={styles.header}>
       <h1>What Music</h1>
-      <label>
-        <input></input>
-        <button></button>
-      </label>
+      <form 
+        name="search"
+        onSubmit={handleSubmit}
+      >
+        <input name="search" type="text"></input>
+        <button type="submit">find</button>
+      </form>
     </header>
   );
+};
+
+Header.propTypes = {
+  setSearchTerm: PropTypes.func
 };
 
 export default Header;
