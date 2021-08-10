d6748110e4ad6102646c8ce8df64d79cfc8a47e0
culi <culitif@tuta.io>
2021-08-10 15:35:32 -0700

    load list of artists

--- src/components/results/ArtistList.jsx
+++ src/components/results/ArtistList.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useArtists } from '../../state/state.js';
+import Header from '../app/Header';
 
 const ArtistList = () => {
-  return <div></div>;
+
+  const { artists, loading, setSearchTerm } = useArtists();
+
+  return <>
+    <Header setSearchTerm={setSearchTerm} />
+
+    {!loading && <ul>
+      {artists.map(artist => <li key={artist.id}>
+        <span>{artist.name}</span>
+        <span>{artist.disambiguation}</span>
+      </li>)}
+    </ul>}
+  </>;
 };
 
 export default ArtistList;
