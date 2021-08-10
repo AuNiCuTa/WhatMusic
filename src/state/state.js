1c05a14caeb3c02a636898d2db0a65c9bc9da9f0
culi <culitif@tuta.io>
2021-08-10 15:34:22 -0700

    write useArtists

--- src/state/state.js
+++ src/state/state.js
@@ -1,1 +1,21 @@
-// do state here
+import React, { useEffect, useState } from 'react';
+import { fetchArtists } from '../services/musicBrainz.js';
+
+const useArtists = () => {
+  const [artists, setArtists] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    searchTerm && fetchArtists(searchTerm)
+      .then(res => setArtists(res))
+      .then(() => setLoading(false))
+      .catch(err => console.log(err))
+    ;
+  }, [searchTerm]);
+
+
+  return { artists, loading, setSearchTerm };
+};
+
+export { useArtists };
