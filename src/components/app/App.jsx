88151ad9465eaae4957b607cd34eea9d5b1e7882
culi <culitif@tuta.io>
2021-08-10 15:34:54 -0700

    set up router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import Header from './Header';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Redirect 
+} from 'react-router-dom';
+import ArtistList from '../results/ArtistList';
 
 export default function App() {
   return (
     <Router>
-      <Header />
+
       <Switch>
-        <Route exact to="/" />
+        <Route exact to="/" render={
+          routerProps => <ArtistList {...routerProps} />
+        }/>
+
+        <Route exact to="/artists/:id" />
+
+        <Route exact to="/releases/:id" />
+
+        <Route exact to="/recordings/:id" />
+
+        <Redirect to="/" />
       </Switch>
+      
     </Router>
   );
 }
