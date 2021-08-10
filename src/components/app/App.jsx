a8e249f3a30b5be9114e71ed414bed4780581d6c
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-10 13:31:16 -0700

    added header with styling

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact to="/" />
+      </Switch>
+    </Router>
+  );
 }
