857aeba248b5269443df2ed92287b13b864acac9
culi <culitif@tuta.io>
2021-08-10 15:36:30 -0700

    nvm on the search component idea

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './header.css';
-import Search from './Search';
 
 const Header = props => {
 
--- src/state/state.js
+++ src/state/state.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchArtists } from '../services/musicBrainz.js';
 
 const useArtists = () => {
