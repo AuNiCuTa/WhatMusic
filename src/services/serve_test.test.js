38d49bda642a704e10ea986260ab397cf532dfe8
culi <culitif@tuta.io>
2021-08-12 09:17:19 -0700

    fix test

--- src/services/serve_test.test.js
+++ src/services/serve_test.test.js
@@ -4,7 +4,7 @@ import {
 
 describe('musicBrainz routes', () => {
   it('fetches Korn', async () => {
-    const Korn = await fetchArtists('Korn');
+    const Korn = (await fetchArtists('Korn')).artists;
 
     expect(Korn[0].id).toEqual('ac865b2e-bba8-4f5a-8756-dd40d5e39f46');
   });
@@ -15,7 +15,5 @@ describe('musicBrainz routes', () => {
 
     expect(releases[0].id).toEqual('01217f30-c009-45cc-847c-43fc3ef8c883');
   });
-
-
 
 });
